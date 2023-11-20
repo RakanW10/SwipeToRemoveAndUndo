99dfaf7386265f4c2aab0cd7d251ac326b4a1e80
rakan <Rakan.w10@outlook.com>
2023-11-20 07:43:18 +0300

    Initial Commit

---
+++ SwipeToRemoveAndUndo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwipeToRemoveAndUndo
+//
+//  Created by Rakan Alotaibi on 06/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwipeToRemoveAndUndo/SwipeToRemoveAndUndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeToRemoveAndUndoApp.swift
+//  SwipeToRemoveAndUndo
+//
+//  Created by Rakan Alotaibi on 06/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct SwipeToRemoveAndUndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
